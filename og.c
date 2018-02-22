ecf6a9cbd5b08625cbcbe0f3e0bcb45dc6dcdfd2
zackkitzmiller <zackkitzmiller@gmail.com>
2018-02-22 14:14:15 -0500

    arg

--- og.c
+++ og.c
@@ -1,12 +1,16 @@
-int og[4] = {0, 1, 2, 3};
-int newfag[2] = {4, 5};
+#include <stdio.h>
+#include <stdlib.h>
 
-float og_size = sizeof(og) / sizeof(og[0]);
-float newfag_size = sizeof(newfag) / sizeof(newfag[0]);
+int main(int argc, char* argv[]) {
+    int og[4] = {0, 1, 2, 3};
+    int newfag[2] = {4, 5};
 
-int total_size = 6;
-float *total = malloc(total_size * sizeof(int));
+    float og_size = sizeof(og) / sizeof(og[0]);
+    float newfag_size = sizeof(newfag) / sizeof(newfag[0]);
 
-memcpy(total, og, (og_size * sizeof(og[0])));
-memcpy(total, newfag, newfag_size * sizeof(newfag[0]));
+    int total_size = 6;
+    float *total = malloc(total_size * sizeof(int));
 
+    memcpy(total, og, (og_size * sizeof(og[0])));
+    memcpy(total, newfag, newfag_size * sizeof(newfag[0]));
+}
